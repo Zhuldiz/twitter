9ef7dfe10aef37031e50f8bf93b62b07393838b7
Zhuldiz <zhuldiz780@gmail.com>
2014-08-15 10:22:28 +0000

    stili foto i user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-   
+    def new
+     @user = User.new
+  end
+  
   def show
     @user=User.find(params[:id]) #ищим пользователя по ИД
   end
   
-  def new
-     @user = User.new
-  end
   
   def create
     @user = User.new(user_params)
